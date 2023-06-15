0408ac34e5af6fab600e2c2c166c4f88b5cdc971
PatelBhargi <133199263+PatelBhargi@users.noreply.github.com>
2023-06-15 20:59:24 +0530

    Add files via upload

---
+++ supe2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+
+{
+	FILE *p;
+	char data[1000];
+
+	p=fopen("bha.txt","w");
+	if(p==NULL)
+	{
+		printf("THIS FILE IS COULD NOT OPEN.\n");
+	}
+	else
+	{
+		printf("ENTER DATA YOU WANT TO INPUT IN TEXT FILE:-");
+		gets(data);
+		fputs("\n",p);
+		fputs(data,p);
+		printf("DATA ADD SUCSSEFULY..........");
+	}
+	fclose(p);
+}
